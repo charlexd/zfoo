0b87a135da0b3a83c77e05ac080157775a800850
denglichuan <denglichuan@elex-tech.com>
2022-06-20 15:10:59 +0800

    add protogen module

---
+++ protogen/src/main/java/ProtoCodeGenerator.java
@@ -0,0 +1,7 @@
+/************************************************************
+ * ProtoCodeGenerator
+ * @date 2022/6/20  
+ * @author denglichuan@elex-tech.com
+ */
+public class ProtoCodeGenerator {
+}
